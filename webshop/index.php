130497781f2156524dc32fd0005fa265e7cc61b7
tejp <cyberkygen@gmail.com>
2016-09-11 15:44:58 +0200

    index.php - first release

---
+++ webshop/index.php
@@ -0,0 +1,38 @@
+<?php
+include 'header.php'
+?>
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.dsek.se/fotoarkiv/data/1999/Nollningen/Lekar-i-gr%F8ngr%E6set%20och%20sittning/00002850.jpg" alt="nollor">
+      <div class="caption">
+        <h3>Råsa Cheps</h3>
+        <p>Det klassiska orginalet. Gör alla teknologer avundsjuka med det senaste från LTH:s modekatalog!</p>
+        <p>
+            <a href="#" class="btn btn-primary" role="button">-</a>
+            <a href="#" class="btn btn-default" role="button">+</a>
+            <a href="#" class="btn btn-default" role="button">Lägg till</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://queerty-prodweb.s3.amazonaws.com/content/docs//2016/05/483208412-real-estate-tycoon-donald-trump-flashes-the-thumbs-up.jpg.CROP_.promo-xlarge2.jpg" alt="nollor">
+      <div class="caption">
+        <h3>Lila Cheps</h3>
+        <p>En exklusiv variant för att sticka ut ur mängden. Detta lila alternativ lämnar inte en trosa torr.</p>
+        <p>
+            <a href="#" class="btn btn-primary" role="button">-</a>
+            <a href="#" class="btn btn-default" role="button">+</a>
+            <a href="#" class="btn btn-default" role="button">Lägg till</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
