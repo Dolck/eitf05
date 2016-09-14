d0922a233af3f2e5b40e20c6c618b8aa29a2d479
Alexander Olsson <alex.olsson.93@gmail.com>
2016-09-14 11:21:39 +0200

    Register now adds users to the database

--- webshop/regUser.php
+++ webshop/regUser.php
@@ -1,5 +1,28 @@
-<!-- handle reg user post request here -->
+<?php
+// handle reg user post request here
+require_once('database.php');
+$db = Database::getInstance();
+$pdo_conn = $db->getConnection();
 
-<?php 
-	var_dump($_POST)
+$username = $_POST['inputUsername'];
+$password = $_POST['inputPassword'];
+$email = $_POST['inputEmail'];
+$forname = $_POST['inputForename'];
+$lastname = $_POST['inputLastname'];
+$city = $_POST['inputCity'];
+$street =$_POST['inputStreet'];
+$zipcode =  $_POST['inputZip'];
+
+$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+try {
+		$stmt = $pdo_conn->prepare($sql);
+		$stmt->execute(array($username, $hashedPassword, $email, $forname, $lastname, $city, $street, $zipcode));
+		$count = $stmt->rowCount();
+	} catch (PDOException $e) {
+		$error = "*** Internal error: " . $e->getMessage() . "<p>" . $query;
+		die($error);
+	}
 ?>
