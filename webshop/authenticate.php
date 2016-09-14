26dcdb13dbae6b4be8d427eb75fb95923ba347dd
Dolck <tim.dolck@gmail.com>
2016-09-14 10:50:37 +0200

    naive authenticate. Some more todo here

--- webshop/authenticate.php
+++ webshop/authenticate.php
@@ -1,1 +1,24 @@
-<!-- handle authenticate user post request here -->
+
+<?php 
+	/* Authenticates user */
+
+	require_once('database.php');
+	session_start();
+	$db = Database::getInstance();
+	$pdo_conn = $db->getConnection(); 
+
+	$usr = $_POST['inputUsername'];
+	$pwd = $_POST['inputPassword'];
+
+	$stmt = $pdo_conn->prepare("select * from users where username=?;");
+	$stmt->execute(array($usr));
+	$result = $stmt->fetchAll();
+	
+
+	if(count($result) == 1){
+		echo "success!";
+	}else{
+		echo "uhm no, just no";
+	}
+
+?>
