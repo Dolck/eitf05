842d763d630324a36a9ed4b73396f74beeee1f9e
emilhasslof <emil.hasslof@live.se>
2016-09-21 09:49:08 +0200

    Password match check in user registration

--- webshop/regUser.php
+++ webshop/regUser.php
@@ -6,6 +6,7 @@ $pdo_conn = $db->getConnection();
 
 $username = $_POST['inputUsername'];
 $password = $_POST['inputPassword'];
+$confirmPassword = $_POST['confirmPassword'];
 $email = $_POST['inputEmail'];
 $forname = $_POST['inputForename'];
 $lastname = $_POST['inputLastname'];
@@ -17,23 +18,29 @@ $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
 $sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-try {
-		$stmt = $pdo_conn->prepare($sql);
-		$stmt->execute(array($username, $hashedPassword, $email, $forname, $lastname, $city, $street, $zipcode));
-		$count = $stmt->rowCount();
-		echo "Successfully created user, returning to homepage!";
+if($password == $confirmPassword){
+	try {
+			$stmt = $pdo_conn->prepare($sql);
+			$stmt->execute(array($username, $hashedPassword, $email, $forname, $lastname, $city, $street, $zipcode));
+			$count = $stmt->rowCount();
+			echo "Successfully created user, returning to homepage!"; ?>
+			<script type="text/javascript">
+			//Redirect after a short time
+			window.setTimeout(function function_name(argument) {
+				window.location = "index.php";
+			}, 3000);
+			</script>
+<?php
+		} catch (PDOException $e) {
+			echo "Unable to create user";
+			$error = "Unable to create user";
+			die($error);
+		}
+	} else {
+		echo "Sorry, it seems like the passwords don't match, please " .
+		"<a href=\"javascript:history.go(-1)\">go back</a>" . " and try again";
 
-	} catch (PDOException $e) {
-		echo "Unable to create user";
-		$error = "Unable to create user";
-		die($error);
 	}
 
 
 ?>
-<script type="text/javascript">
-//Redirect after a short time
-window.setTimeout(function function_name(argument) {
-	window.location = "index.php";
-}, 3000);
-</script>
